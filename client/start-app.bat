e6c813d4e03dcc2691fcfaa80c167f0e046e5f59
AchrafMenach <menachachraf3@gmail.com>
2025-08-15 20:28:30 +0100

    quelque modif

---
+++ client/start-app.bat
@@ -0,0 +1,7 @@
+@echo off
+echo =========================================
+echo   Lancement de l'application scolaire
+echo =========================================
+npm install
+echo =========================================
+echo   Application arrêtée
